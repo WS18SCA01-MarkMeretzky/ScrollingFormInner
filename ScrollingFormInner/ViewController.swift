7a84f7c8fc6162c03b80a12bdfb90dc97b9e09fe
Mark Meretzky <mark.meretzky@gmail.com>
2019-02-05 23:43:09 -0500

    Commit.

--- ScrollingFormInner/ViewController.swift
+++ ScrollingFormInner/ViewController.swift
@@ -10,11 +10,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!;
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
+        super.viewDidLoad();
         // Do any additional setup after loading the view, typically from a nib.
+         registerForKeyboardNotifications();
+    }
+    
+    //MARK: Keyboard Notifications
+    
+    func registerForKeyboardNotifications() {
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWasShown(_:)),
+            name: UIResponder.keyboardDidShowNotification,
+            object: nil);
+        
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillBeHidden(_:)),
+            name: UIResponder.keyboardWillHideNotification,
+            object: nil);
+    }
+    
+    @objc func keyboardWasShown(_ notification: NSNotification) {
+        guard let info: [AnyHashable: Any] = notification.userInfo,
+            let keyboardFrameValue: NSValue = info[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue else {
+                return;
+        }
+        
+        let height: CGFloat = keyboardFrameValue.cgRectValue.size.height;
+        let contentInsets: UIEdgeInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: height, right: 0.0);
+        scrollView.contentInset = contentInsets;
+        scrollView.scrollIndicatorInsets = contentInsets;
+    }
+    
+    @objc func keyboardWillBeHidden(_ notification: NSNotification) {
+        scrollView.contentInset = .zero;
+        scrollView.scrollIndicatorInsets = .zero;
     }
 
-
+    @IBAction func returnKeyTapped(_ sender: UITextField) {
+        sender.resignFirstResponder();
+    }
+    
 }
 
